537e73998d1a01b392d9ac1a4229b390c9acd8a8
hongseok choi <baw6114@gmail.com>
2020-06-07 16:38:36 +0900

    리스트란 순차적인 데이터 구조

--- Data_Structure_CHS/Ch03/ListMain.c
+++ Data_Structure_CHS/Ch03/ListMain.c
@@ -6,35 +6,40 @@ int main(void)
 	/*** ArrayList의 생성 및 초기화 ***/
 	List list;
 	int data;
+	int sumdata = 0;
 	ListInit(&list);
 
-	/*** 5개의 데이터 저장 ***/
-	LInsert(&list, 11);  LInsert(&list, 11);
-	LInsert(&list, 22);  LInsert(&list, 22);
-	LInsert(&list, 33);
-
-	/*** 저장된 데이터의 전체 출력 ***/
-	printf("현재 데이터의 수: %d \n", LCount(&list));
-
-	if(LFirst(&list, &data))    // 첫 번째 데이터 조회
-	{
-		printf("%d ", data);
-		
-		while(LNext(&list, &data))    // 두 번째 이후의 데이터 조회
-			printf("%d ", data);
+	/*** 1 ~ 9까지의 정수 저장 ***/
+	for(int i = 1; i < 10; i++){
+		LInsert(&list, i);
 	}
+
+	/*** 저장된 데이터의 전체 합 출력 ***/
+	printf("전체 데이터의 개수: %d \n", LCount(&list));
+
+	LFirst(&list, &data);
+	for(int i = 0; i < 9; i++){
+		sumdata += data;
+		printf("%d ", data);
+		LNext(&list, &data);
+	}
+	printf("\n전체 데이터의 합: %d\n", sumdata);
 	printf("\n\n");
 
-	/*** 숫자 22을 탐색하여 모두 삭제 ***/
+	/*** 2, 3의 배수 삭제 ***/
 	if(LFirst(&list, &data))
 	{
-		if(data == 22)
+		if(data%2 == 0 || data%3 == 0)
+		{
 			LRemove(&list);
+		}
 		
 		while(LNext(&list, &data))
 		{
-			if(data == 22)
+			if(data%2 == 0 || data%3 == 0)
+			{
 				LRemove(&list);
+			}
 		}
 	}
 
